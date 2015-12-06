d0dc3948427edd903268efc315d5d1c8c99229a3
Joris Lammers <joris.lammers.be@gmail.com>
2015-12-06 16:05:29 +0100

    Reworked to use miekg's pcap directly
    Required some changes as the pcap API changed between my first version
    and this one

--- ExtractUdpRtpPayload.go
+++ ExtractUdpRtpPayload.go
@@ -3,30 +3,35 @@ package main
 import (
 	"fmt"
 	"os"
-	"pcap"
+
+	"github.com/miekg/pcap"
 )
 
 func main() {
-	var Destinations map[string]*os.File = make(map[string]*os.File)
+	Destinations := make(map[string]*os.File)
 	fmt.Println(os.Args)
-	for _, f := range os.Args[1 : len(os.Args)] {
-		h, _ := pcap.Openoffline(f)
+	for _, f := range os.Args[1:len(os.Args)] {
+		h, _ := pcap.OpenOffline(f)
 
 		for pkt := h.Next(); pkt != nil; pkt = h.Next() {
 			pkt.Decode()
-			if pkt.UDP != nil {
-				dumpFileName := fmt.Sprintf("Dump_%d.%d.%d.%d_%d.ts", pkt.IP.DestIp[0],
-					pkt.IP.DestIp[1],
-					pkt.IP.DestIp[2],
-					pkt.IP.DestIp[3],
-					pkt.UDP.DestPort)
-				f, ok := Destinations[dumpFileName]
-				if !ok {
-					fmt.Println("Creating new TS file", dumpFileName)
-					Destinations[dumpFileName], _ = os.Create(dumpFileName)
-					f, _ = Destinations[dumpFileName]
+			if len(pkt.Headers) >= 2 {
+				ipH, ipOK := pkt.Headers[0].(*pcap.Iphdr)
+				udpH, udpOK := pkt.Headers[1].(*pcap.Udphdr)
+				if ipOK && udpOK {
+					dumpFileName := fmt.Sprintf("Dump_%d.%d.%d.%d_%d.ts", ipH.DestIp[0],
+						ipH.DestIp[1],
+						ipH.DestIp[2],
+						ipH.DestIp[3],
+						udpH.DestPort)
+					f, ok := Destinations[dumpFileName]
+					if !ok {
+						fmt.Println("Creating new TS file", dumpFileName)
+						Destinations[dumpFileName], _ = os.Create(dumpFileName)
+						f, _ = Destinations[dumpFileName]
+					}
+					f.Write(pkt.Payload)
 				}
-				f.Write(pkt.Payload)
 			}
 		}
 	}
